e2331cda3e051d188568e842406c871863aaa803
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-06-02 23:20:46 +0530

    {server}: middleware done

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,10 @@
+"use server";
+
+import { auth } from "@/auth";
+
+const SettingsPage = async () => {
+  const session = await auth();
+  return <div>{JSON.stringify(session)}</div>;
+};
+
+export default SettingsPage;
